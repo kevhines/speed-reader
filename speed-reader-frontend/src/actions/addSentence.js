6585678d0ca3414096826784cc027cc8022a5164
Kevin <kevhines@gmail.com>
2021-06-01 17:24:15 -0400

    minor edits

--- speed-reader-frontend/src/actions/addSentence.js
+++ speed-reader-frontend/src/actions/addSentence.js
@@ -9,13 +9,11 @@ return function(dispatch) {
 
 
       // return function(dispatch) {
-    //   console.log("5/4 top of thunk in addlist") //4
     //   fetch("http://localhost:3001/lists", {
     //     method: "POST",
     //     headers: {Accept: "application/json", "Content-Type": "application/json"},
     //     body: JSON.stringify({list: list})
     //   }).then(r => r.json())
-    //   .then(list => dispatch({type: "ADD_LIST", payload: list}))
-    //     console.log("7/6 bottom of thunk in addlist") //5
+    //   .then(sentence => dispatch({type: "ADD_SENTENCE", payload: sentence}))
     // }
   
