a13dae06cc9272869fd7199444b65c089e75bb6a
Kevin <kevhines@gmail.com>
2021-06-05 16:57:38 -0400

    made a create controller route in backend

--- speed-reader-backend/app/controllers/scores_controller.rb
+++ speed-reader-backend/app/controllers/scores_controller.rb
@@ -1,2 +1,19 @@
 class ScoresController < ApplicationController
+
+    def create
+        score = Score.new(score_params)
+        if user.save
+            render json: score
+        else
+            render json: {message: "Score Didn't Save!"}
+        end
+    end
+
+    
+    private
+    
+    def user_params
+        params.require(:score).permit(:user_id, :percent)
+    end
+
 end
